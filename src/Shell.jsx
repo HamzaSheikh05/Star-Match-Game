82a947436b31f02b666ebca2743bae23b7b86bb5
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-06-17 22:09:00 +0400

    Created a function to change the color of the button based on logic, Hardcoded

--- src/Shell.jsx
+++ src/Shell.jsx
@@ -3,7 +3,12 @@ import { useState } from 'react';
 import './ShellCss.css';
 
 const PlayNumber = props => (
-    <button className="number" onClick={() => console.log("Num", props.number)}>{props.number}</button> 
+    <button 
+    className="number" 
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => console.log("Num", props.number)}>
+        {props.number}
+    </button> 
 );
 
 const StarsDisplay = props => (
@@ -16,6 +21,20 @@ const StarsDisplay = props => (
 
 const StarMatch = () => {
     const [stars, setStars] = useState(utils.random(1,9));
+    const [availableNums, setAvailableNums] = useState([1,2,3,4,5]);
+    const [candidateNums, setCandidateNums] = useState([2,3]);
+
+    const candidatesAreWrong = utils.sum(candidateNums) > stars;
+
+    const numberStatus = (number) => {
+        if(!availableNums.includes(number)){
+            return "used";
+        }
+        if(candidateNums.includes(number)){
+            return candidatesAreWrong ? 'wrong': 'candidate';
+        }
+        return "available";
+    };
     return(
         <div className="game">
             <div className="help">
@@ -29,7 +48,11 @@ const StarMatch = () => {
 
                 <div className="right">
                     {utils.range(1,9).map(number => 
-                        <PlayNumber key={number} number= {number}/>    
+                        <PlayNumber 
+                        key={number}
+                        status={numberStatus(number)}
+                        number= {number}
+                        />    
                     )}
                 </div>
             </div>
